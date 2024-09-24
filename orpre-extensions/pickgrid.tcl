63fe72cba9051096db3adf62a789103dd8f7e194
Gianmarco Dogliani <84925559+gianmarcodogliani@users.noreply.github.com>
2024-09-24 10:14:19 +0200

    Add x, ix, iy commands

--- orpre-extensions/pickgrid.tcl
+++ orpre-extensions/pickgrid.tcl
@@ -1,7 +1,7 @@
-# Paste these procedure inside OrCAD X Presto's Command Window to use Allegro-like syntax for drawing commands
+# Paste these procedure inside OrCAD X Presto's Command Window to use Allegro-like syntax for drawing commands.
 
-proc x {x y} {variable p; set p "$x $y"; puts "pick grid $x $y"}
+proc x {x y} {variable p; set p "$x $y"; pcb::cmd "pick grid $x $y"}
 
-proc ix {os} {variable p; set p "[expr [lindex $p 0] + $os] [lindex $p 1]"; puts "pick grid [lindex $p 0] [lindex $p 1]"}
+proc ix {os} {variable p; set p "[expr [lindex $p 0] + $os] [lindex $p 1]"; pcb::cmd "pick grid [lindex $p 0] [lindex $p 1]"}
 
-proc iy {os} {variable p; set p "[lindex $p 0] [expr [lindex $p 1] + $os]"; puts "pick grid [lindex $p 0] [lindex $p 1]"}
+proc iy {os} {variable p; set p "[lindex $p 0] [expr [lindex $p 1] + $os]"; pcb::cmd "pick grid [lindex $p 0] [lindex $p 1]"}
